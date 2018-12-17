38c6bc37ce62e8307e14bfe6a4c0ec920afb1b4e
Ezequiel Contreras <ezequiel.contreras@flex.com>
2018-12-17 12:01:33 -0800

    Added Postgres DB and env var credentials

--- web_project/settings.py
+++ web_project/settings.py
@@ -77,13 +77,16 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': os.environ['postgres_un'],
+        'PASSWORD': os.environ['postgres_un'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
+} 
 
 
 # Password validation
